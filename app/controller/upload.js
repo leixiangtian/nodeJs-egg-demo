dcd6fdf3d77441da9d706db9e485aa1fb14936c5
leixiangtian <v_vxtlei@tencent.com>
2021-06-17 19:07:57 +0800

    update

--- app/controller/upload.js
+++ app/controller/upload.js
@@ -4,6 +4,7 @@ const request = require('request');
 const Controller = require('egg').Controller;
 const COS = require('cos-nodejs-sdk-v5');
 const toArray = require('stream-to-array');
+const fs = require('fs');
 
 const config = {
   SecretId: 'AKID0pOhjVzC8nCKBl58uQgeLlumMfhSw95k',
@@ -32,48 +33,51 @@ class UploadController extends Controller {
         fileSize: '',
         fileUrl: '',
         sha256: '',
-      }
+      },
+      msg: '',
     };
-
     return new Promise((resolve, reject) =>{
       // 单片上传
-      cos.putObject({
-        Bucket: config.Bucket, /* Bucket,名称 必须 */
-        Region: config.Region,    /* 所属地域 必须 */
-        Key: `${config.path}/${appId}/${stream.filename}`,            /* 必须 */
-        Body: buf, // 上传文件对象
-        // Body: fs.readFileSync(filepath), // 上传文件对象
+      // cos.putObject({
+      //   Bucket: config.Bucket, /* Bucket,名称 必须 */
+      //   Region: config.Region,    /* 所属地域 必须 */
+      //   Key: `${config.path}/${appId}/${stream.filename}`,            /* 必须 */
+      //   Body: buf, // 上传文件对象
+      //   // Body: fs.readFileSync(filepath), // 上传文件对象
+      // }, function(err, data) {
+      //   // console.log(err, data);
+      //   if (err) {
+      //     reject(err);
+      //   } else {
+      //     formData.code = 0;
+      //     formData.fileInfo.fileUrl = `https://${data.Location}`;
+      //     formData.msg = 'success';
+      //     resolve(formData);
+      //   }
+      // });
+
+      // 分片上传
+      cos.sliceUploadFile({
+        Bucket: config.Bucket,
+        Region: config.Region,
+        Key: `${config.path}/${appId}/${stream.filename}`,
+        FilePath: './',
+        Body: stream,
       }, function(err, data) {
-        // console.log(err, data);
         if (err) {
           reject(err);
         } else {
           formData.code = 0;
           formData.fileInfo.fileUrl = `https://${data.Location}`;
-          console.log(stream)
+          formData.msg = 'success';
           resolve(formData);
         }
       });
-
-      // 分片上传
-      // cos.sliceUploadFile({
-      //   Bucket: config.Bucket,
-      //   Region: config.Region,
-      //   Key: 'file/' + stream.filename,
-      //   FilePath: 'file/',
-      //   // Body: fs.createReadStream(stream.filepath),
-      // }, function(err, data) {
-      //   // console.log(err, data);
-      //   // if (err) reject(err);
-      //   // else resolve(data);
-      //   resolve(err || data);
-      // });
     })
   }
 
   async upload() {
     const result = await this.cosSdkUpload();
-    
     this.ctx.body = result;
   }
 }
